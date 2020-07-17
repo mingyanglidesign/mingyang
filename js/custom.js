7b368ab0dbb732546486b9737f354b988c7224b0
mingyanglidesign <36492886+mingyanglidesign@users.noreply.github.com>
2020-07-17 14:39:41 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+(function ($) {
+
+    "use strict";
+
+        // PRE loader
+        $(window).load(function(){
+          $('.preloader').fadeOut(1000); // set duration in brackets    
+        });
+
+
+        //Navigation Section
+        $('.navbar-collapse a').on('click',function(){
+          $(".navbar-collapse").collapse('hide');
+        });
+
+        $(window).scroll(function() {
+          if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+              } else {
+                $(".navbar-fixed-top").removeClass("top-nav-collapse");
+              }
+        });
+
+
+        // Smoothscroll js
+        $(function() {
+          $('.custom-navbar a, #home a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 49
+            }, 1000);
+            event.preventDefault();
+          });
+        });  
+
+
+        // WOW Animation js
+        new WOW({ mobile: false }).init();
+
+})(jQuery);
+
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
